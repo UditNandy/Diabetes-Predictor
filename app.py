fdd66524d9d24d0b8a606f0f7d3e3a24bd5fb25d
UditNandy <63676516+UditNandy@users.noreply.github.com>
2021-05-06 01:32:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,22 +18,22 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        ls=[x for x in request.form.values()]
 
-        df1= {'Pregnancies': ls[0], 'Glucose': ls[1], 'BloodPressure': ls[2], 'SkinThickness': ls[3], 'Insulin': ls[4], 'BMI': ls[5],
+    ls=[x for x in request.form.values()]
+
+    df1= {'Pregnancies': ls[0], 'Glucose': ls[1], 'BloodPressure': ls[2], 'SkinThickness': ls[3], 'Insulin': ls[4], 'BMI': ls[5],
               'DiabetesPedigreeFunction': ls[6], 'Age': ls[7]}
 
-        df1=df.append(df1, ignore_index=True)
-        d = scaler.fit_transform(df1)
-        l = d[d.shape[0] - 1, :]
-        l1 = np.reshape(l,(1,8))
-        df.drop(df.shape[0] - 1, inplace=True)
-        my_prediction = model.predict(l1)
-        if(my_prediction[0]==1):
-            return render_template('index.html',prediction_text='You are having Diabetes')
-        else:
-            return render_template('index.html', prediction_text='You are not having diabetes')
+    df1=df.append(df1, ignore_index=True)
+    d = scaler.fit_transform(df1)
+    l = d[d.shape[0] - 1, :]
+    l1 = np.reshape(l,(1,8))
+    df.drop(df.shape[0] - 1, inplace=True)
+    my_prediction = model.predict(l1)
+    if(my_prediction[0]==1):
+        return render_template('index.html',prediction_text='You are having Diabetes')
+    else:
+        return render_template('index.html', prediction_text='You are not having diabetes')
 
 
 if __name__ == "__main__":
